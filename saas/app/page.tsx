8b866753bfe0d22c6f40c4844ffad99e528ce5e5
Kishor Naik <kishor.naik011.net@gmail.com>
2025-06-03 09:47:31 +0530

    Dashboard Added

--- saas/app/page.tsx
+++ saas/app/page.tsx
@@ -1,16 +1,35 @@
 import React from 'react'
 import {Button} from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionList from "@/components/CompanionList";
+import CTA from "@/components/CTA";
+import {recentSessions} from "@/constants";
 
 const Page = () => {
   return (
-    <div>
+    <main>
       <h1 className="text-2xl underline">
-        Welcome to SasS App
+        Popular Companions
       </h1>
-        <Button>
-          Let's get started.            
-        </Button>
-    </div>
+        <section className="home-section">
+            <CompanionCard
+                id={1}
+                name="Companion 1"
+                topic="Topic 1"
+                subject="Subject 1"
+                duration={60}
+                color="#FF0000"
+            />
+        </section>
+        <section className="home-section">
+            <CompanionList
+                title="Recently completed sessions"
+                companions={recentSessions}
+                className="w-2/3 max-lg:w-full"
+            />
+            <CTA/>
+        </section>
+    </main>
   )
 }
 
