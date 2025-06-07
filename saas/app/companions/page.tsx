2417a226f9d5c97e9bc126260a3b171210e451ab
Kishor Naik <kishor.naik011.net@gmail.com>
2025-06-07 19:37:58 +0530

    Vapi Setup Added

--- saas/app/companions/page.tsx
+++ saas/app/companions/page.tsx
@@ -1,9 +1,39 @@
-const companionsLibraryPage=()=>{
+import {getAllCompanions} from "@/lib/actions/companion.actions";
+import CompanionCard from "@/components/CompanionCard";
+import {getSubjectColor} from "@/lib/utils";
+import SearchInput from "@/components/SearchInput";
+import SubjectFilter from "@/components/SubjectFilter";
+
+
+const companionsLibraryPage=async ({searchParams}:SearchParams)=>{
+
+    const filters=await searchParams;
+    const subject=filters.subject ? filters.subject : "";
+    const topic=filters.topic ? filters.topic : "";
+
+    const companions=await getAllCompanions({subject, topic});
+
+    console.log(companions);
+
     return (
         <>
-            <div>
-                Companions Library
-            </div>
+            <main>
+                <section className="flex justify-between gap-4 max-sm:flex-col">
+                    <h1>Companions Library</h1>
+                    <div className="flex gap-4">
+                        <SearchInput/>
+                        <SubjectFilter/>
+                    </div>
+                </section>
+                <section className="companions-grid">
+                    {companions.map((companion)=>(
+                        <CompanionCard key={companion.id}
+                                       color={getSubjectColor(companion.subject)}
+                                       {...companion}
+                        />
+                    ))}
+                </section>
+            </main>
         </>
     )
 }
