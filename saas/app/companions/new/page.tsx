93f6d1b2787e6283d88fbbc6081c87f8b9e32970
Kishor Naik <kishor.naik011.net@gmail.com>
2025-06-11 19:42:25 +0530

    Update Next Config

--- saas/app/companions/new/page.tsx
+++ saas/app/companions/new/page.tsx
@@ -1,6 +1,9 @@
 import CompanionForm from "@/components/CompanionForm";
 import {auth} from "@clerk/nextjs/server";
 import {redirect} from "next/navigation";
+import {newCompanionPermissions} from "@/lib/actions/companion.actions";
+import Image from "next/image";
+import Link from "next/link";
 
 const NewCompanionsPage=async ()=>{
 
@@ -9,15 +12,39 @@ const NewCompanionsPage=async ()=>{
     if(!userId)
         redirect(`/sign-in`);
 
+    const canCreateCompanion=await newCompanionPermissions();
+
+
     return (
         <>
             <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
-                <article className="w-full flex flex-col gap-4">
-                    <h1>
-                        Companion Builder
-                    </h1>
-                    <CompanionForm/>
-                </article>
+                {canCreateCompanion
+                    ? (
+                        <article className="w-full flex flex-col gap-4">
+                            <h1>
+                                Companion Builder
+                            </h1>
+                            <CompanionForm/>
+                        </article>
+                    )
+                    :
+                    (
+                        <article className="companion-limit">
+                            <Image src="/images/limit.svg" alt="Companion limit reached" width={360} height={230}/>
+                            <div className="cta-badge">
+                                Upgrade Your Plan
+                            </div>
+                            <h1>
+                                You have reached your companion limit
+                            </h1>
+                            <p>
+                                You have reached your companion limit. Upgrade your plan to add more companions.
+                            </p>
+                            <Link href="/subscription" className="btn-primary w-full justify-center">Upgrade My Plan</Link>
+                        </article>
+                    )
+                }
+
             </main>
         </>
     )
